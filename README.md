7d3dfd961ee921e5fcd183b46c8406716d0cc7d2
ChristianCella <christian.cella@polimi.it>
2024-12-28 11:23:26 +0100

    Added the readme

--- README.md
+++ README.md
@@ -1,52 +1,70 @@
-# Quest2ROS
-Ros code to connect the Occlus quest to ROS and receive the controller Position/velocities and send haptic feedback to the controllers.
+# Quest2ROS  <img src="https://cdn-icons-png.flaticon.com/128/4821/4821613.png" alt="my image" width="25"/>
 
+This repo is a fork from the original one that can be found at:
+- https://github.com/Quest2ROS/quest2ros
 
-## setup
+The scripts in this repo allow to connect the Occlus quest to ROS and receive the controller Position/velocities and send haptic feedback to the controllers. To use the original repo linked above you can checkout to the main branch of this one instead.
 
-1. Install Quest2ROS on your oculus VR headset [https://quest2ros.github.io/q2r-web/](https://quest2ros.github.io/q2r-web/)
+---
 
-2. Clone [ROS TCP enpoint](https://github.com/Unity-Technologies/ROS-TCP-Endpoint) into your catkin workspace src
+### **Installation procedure** <a name="install"></a> 🏗️
 
-`git clone https://github.com/Unity-Technologies/ROS-TCP-Endpoint.git`
+1. Install Quest2ROS on your oculus VR headset:
+ [https://quest2ros.github.io/q2r-web/](https://quest2ros.github.io/q2r-web/)
 
-3. Clone quest2ros ROS package in your catkin workspace src
+2. Clone [ROS TCP enpoint](https://github.com/Unity-Technologies/ROS-TCP-Endpoint) into your catkin workspace src:
 
-`git clone https://github.com/Quest2ROS/quest2ros.git`
+    ```
+    cd (your_path_to_ws)
+    git clone https://github.com/Unity-Technologies/ROS-TCP-Endpoint.git
+    ```
 
-4. Build your catkin workspace
+3. Clone quest2ros ROS package in your catkin workspace src:
 
-`catkin build`
+    ```
+    https://github.com/ChristianCella/quest2ros.git
+    ```
 
-5. Make sure ROS PC and Oculus Headset are on the same (WIFI) network
+4. Build your catkin workspace:
 
-6. Start ROS TCP endpoint (replace <YOUR_IP>)
+    `catkin build -cs`
 
-`roslaunch ros_tcp_endpoint endpoint.launch tcp_ip:=<YOUR_IP> tcp_port:=10000`
+5. Make sure ROS PC and Oculus Headset are on the same (Wi-Fi) network. To check the IP address of your Wi-Fi network go in Settings -> IPv4 address.
 
-7. Set <YOUR_IP> and the same port in the VR and press **apply**
+6. Set <YOUR_IP> and the same port in the VR (after selecting the application Quest2Ros) and press **apply** (for example, in my case it was 192.168.1.22).
+
+7. Start ROS TCP endpoint (replace <YOUR_IP>)
+
+    ```
+    roslaunch ros_tcp_endpoint endpoint.launch tcp_ip:=<YOUR_IP> tcp_port:=10000
+    ```
 
 8. You should see [INFO] in the terminal that the connection is established
 
-9. Run ros2quest demo
+9. Run [ros2quest.py](https://github.com/ChristianCella/quest2ros/blob/dev_chris/scripts/ros2quest.py), which allows to write the pose on the toopic '/desired_pose':
 
-`rosrun quest2ros ros2quest.py`
+    ```
+    rosrun quest2ros ros2quest.py
+    ```
+---
 
-10. You can now move the dice and Q2R logo in the VR by pressing the lower controller button on the left and right controller, respectively
-
-## Coordinate frame alignment
+### **Coordinate frame alignment** <a name="alignment"></a> 🖼️
 
 By pressing A + B on the right and X+Y on the left, the relative coordinate frame gets aligned to the current controller position.
 To use this for robot teleop allign the controller with the base frame of the robot and press the buttons.
 
-## Troubleshooting
+---
+
+### **Troubleshooting** <a name="troubleshooting"></a> 🔫
 In case the TCP-endpoint is not connecting to the VR app, try to allow the connection through the port you have specified (10000 in this example) with the following command:
 
 ```
 sudo iptables -I INPUT -p tcp -m tcp --dport 10000 -j ACCEPT
 ```
 
-## Bibtex
+---
+
+### **BibiTex** <a name="bibitex"></a> 📔
 
 ```
 @inproceedings{@software{q2r2023,
--- scripts/ros2quest.py
+++ scripts/ros2quest.py
@@ -10,12 +10,13 @@ import numpy as np
 
 class ros2quest:
 
+  '''Class constructor'''
   def __init__(self):
 
     # Time
     self.last_timestamp = rospy.Time.now()
     
-    # TF broadcaster
+    # TF broadcaster (in this code the 'desired_pose'c frame is published)
     self.br = TransformBroadcaster()
     
     # TF listener  
@@ -28,56 +29,77 @@ class ros2quest:
     self.reference_name="base_link"
     self.tcp_name="tool0"
        
-    # Getting robot's ee for create the new desired_pose reference frame
+    # Try to get the robot end-effector pose, provided it is already available
     try:
       self.base2flange()
     except (LookupException, ConnectivityException):
       rospy.logerr("Exception catched during lookup transform... killing myself")
       rospy.signal_shutdown()
       
-    # Subscribers
+    ''' 
+    Initialize subscribers
+    Subscribe to:
+    - /q2r_right_hand_pose: get the right hand pose
+    - /q2r_right_hand_inputs: get the right hand inputs (buttons, triggers, etc.)
+    '''
     self.ovr2ros_right_hand_pose_sub = rospy.Subscriber("/q2r_right_hand_pose", PoseStamped, self.ovr2ros_right_hand_pose_callback, queue_size=1)
     self.ovr2ros_right_hand_inputs_sub = rospy.Subscriber("/q2r_right_hand_inputs", OVR2ROSInputs, self.ovr2ros_right_hand_inputs_callback, queue_size=1)
     
-    # Publishers  
+    ''' 
+    Initialize publishers
+    Publish to:
+    - /q2r_right_hand_haptic_feedback: send haptic feedback to the right hand
+    - /q2r_left_hand_haptic_feedback: send haptic feedback to the left hand
+    - /desired_pose: send the desired pose to the robot
+    '''
     self.ros2ovr_right_hand_haptic_feedback_pub = rospy.Publisher("/q2r_right_hand_haptic_feedback", OVR2ROSHapticFeedback, queue_size=1)
     self.ros2ovr_left_hand_haptic_feedback_pub = rospy.Publisher("/q2r_left_hand_haptic_feedback", OVR2ROSHapticFeedback, queue_size=1)
     self.desired_pose_pub = rospy.Publisher("/desired_pose", PoseStamped, queue_size=1)
 
+    # Store poses and inputs
     self.right_hand_position = np.zeros(3)
     self.right_hand_orientation = np.zeros(4)
     self.right_hand_inputs = OVR2ROSInputs()
 
-    
-
-  #####################
-  ### Ros callbacks ###
-  #####################
-  # Right hand 
-  def ovr2ros_right_hand_pose_callback(self, data):    
+  '''
+  Class-specific methods
+  '''
+  
+  def ovr2ros_right_hand_pose_callback(self, data): 
+    ''' 
+    Callback function for the right hand pose.
+    Triggered whenever a new PoseStamped message arrives on the /q2r_right_hand_pose topic.
+    '''   
     self.right_hand_position = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
     self.right_hand_orientation = np.array([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
     now = rospy.Time.now()
     
+    # Ensure increasing timestamps
     if now <= self.last_timestamp:
       now = self.last_timestamp + rospy.Duration(1e-9)  # Increment by a nanosecond
     self.last_timestamp = now
     
+    # This desirted pose will be broadcast into TF as 'desired_pose' relative to the reference frame
     desiredPose = geometry_msgs.msg.TransformStamped()
     desiredPose.header.stamp = now
     desiredPose.header.frame_id = self.reference_name
     desiredPose.child_frame_id = "desired_pose"
     
+    # If button 'A' is pressed:
     if self.right_hand_inputs.button_lower == True:
+
+      # 1) Compute the delta in terms of poses
       deltaPosition = self.right_hand_position - self.p_reference
       
-
+      # 2) Compute the delta in terms of orientations
       q_ref_inv = transformations.quaternion_inverse(self.q_reference)
       deltaOrientation = transformations.quaternion_multiply(q_ref_inv, self.right_hand_orientation)
-            
+
+      # 3) Add the deltas to the robot's current base->flange pose  
       desiredPosition = self.base2flangePosition + deltaPosition
       desiredPoseOrientation = transformations.quaternion_multiply(deltaOrientation, self.base2flangeOrientation)
       
+      # 4) Fill in the desired pose
       desiredPose.transform.translation.x = desiredPosition[0]
       desiredPose.transform.translation.y = desiredPosition[1]
       desiredPose.transform.translation.z = desiredPosition[2]  
@@ -86,7 +108,10 @@ class ros2quest:
       desiredPose.transform.rotation.z = desiredPoseOrientation[2]
       desiredPose.transform.rotation.w = desiredPoseOrientation[3]
 
+    # Button 'A' is not pressed
     else:
+
+      # Just set the transform to be equal to the robot's current base->flange pose
       try:
         self.base2flange()
         
@@ -102,9 +127,11 @@ class ros2quest:
       desiredPose.transform.rotation.z = self.base2flangeOrientation[2]
       desiredPose.transform.rotation.w = self.base2flangeOrientation[3]
     
+    # Regardless of the button state, broadcast the desired pose
     self.br.sendTransform(desiredPose)
     
-    # Publishing hand pose to topic 
+    # Publishing hand pose to topic (/desired_pose)
+    # This is the same transform data as 'desired_pose', just in PoseStamped form
     msg = geometry_msgs.msg.PoseStamped()
     msg.header.stamp = now
     msg.header.frame_id = self.reference_name
@@ -118,8 +145,11 @@ class ros2quest:
     self.desired_pose_pub.publish(msg)
     
   def ovr2ros_right_hand_inputs_callback(self, data):
-    
-    # Upper front -> save relative pose
+    ''' 
+    Callback function for the right hand inputs.
+    Triggered whenever a new OVR2ROSInputs message arrives on the /q2r_right_hand_inputs topic.
+    '''
+    # When the button 'A' transitions from not pressed to pressed, store the current right hand pose as the reference
     if self.right_hand_inputs.button_lower == False and data.button_lower == True:
       self.p_reference = self.right_hand_position
       self.q_reference = self.right_hand_orientation
@@ -132,10 +162,15 @@ class ros2quest:
         rospy.logerr("Exception catched during lookup transform")
         return
     
+    # Update the stored right hand inputs
     self.right_hand_inputs = data
     
           
   def base2flange(self):
+    ''' 
+    Get the transformation from the base to the flange.
+    The lookup waits for 10 seconds before throwing an exception.
+    '''
     base2flange = geometry_msgs.msg.TransformStamped()
     base2flange = self.tf_buffer.lookup_transform(self.reference_name, self.tcp_name, rospy.Time(), rospy.Duration(10))
     self.base2flangePosition = np.array([base2flange.transform.translation.x, base2flange.transform.translation.y, base2flange.transform.translation.z])
